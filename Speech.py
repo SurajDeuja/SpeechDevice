67a7b264feb50e263e721ce07464f74bce2c00b6
Suraj Deuja <deujasuraj77@gmail.com>
2016-04-20 21:11:51 -0600

    added thread for speech

--- Speech.py
+++ Speech.py
@@ -1,16 +1,20 @@
 import subprocess
-
+from threading import Thread
 
 class Speech:
     def __init__(self):
         pass
 
-    def speak(self, text):
+    def run_cmd(self, text):
         try:
             proc = subprocess.Popen(["espeak", text])
             proc.communicate()
         except Exception:
             pass
+
+    def speak(self, text):
+        thread = Thread(target=self.run_cmd(text))
+        thread.start()
 
 if __name__ == "__main__":
     speech = Speech()
